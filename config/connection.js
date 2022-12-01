47d7c221e93766bf4aee623f278d5f8327df4ff2
Karina Drummond <k.drummond528@gmail.com>
2022-12-01 09:16:27 -0800

    modified connection.js

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const { connect, connection } = require('mongoose');
+
+const connectionString =
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+module.exports = connection;
